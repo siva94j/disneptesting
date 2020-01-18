1ef39e62c136515e62be6de3805848142361599c
siva94j <siva94j@gmail.com>
2020-01-18 23:44:25 +0530

    Completed Ecommerce project

--- src/main/java/SpiritTest.java
+++ src/main/java/SpiritTest.java
@@ -21,29 +21,41 @@ public void selectTripType(String tripType) {
 		tripRadios.get(2).click();
 	}
 }
+//have to merge origin and destination into single method
 public void selectOrigin(String origin) {
 	Clickon(getWebelement("//input[@id='flight-OriginStationCode']"));
 	List<WebElement> originPlaces = getWebelements("//button[@class='btn btn-link pt-0 pb-0 font-size-dropdown h-unset text-left text-capitalize']");
 	for (WebElement x : originPlaces) {
-		if(x.getText().contains(origin)) {
+		if(x.getText().contains("("+origin+")")) {
 			x.click();
+			break;
 		}
 	}
 }
-public void selectDest(String destination) {
+public void selectDest(String destination){
 	Clickon(getWebelement("//input[@name='destinationStationCode']"));
 	List<WebElement> originPlaces = getWebelements("//button[@class='btn btn-link pt-0 pb-0 font-size-dropdown h-unset text-left text-capitalize']");
 	for (WebElement x : originPlaces) {
-		if(x.getText().contains(destination)) {
+		if(x.getText().contains("("+destination+")")) {
 			x.click();
+			break;
 		}
 	}
 }
-public static void main(String[] args) throws IOException {
+public void selectDate(String date) {
+	String day = date.split("/")[0];
+	String month = date.split("/")[1];
+	String year = date.split("/")[2];
+	Clickon(getWebelement("//input[@name='date']"));
+	Clickon(getWebelement("//button[@class='current']"));
+	
+}
+public static void main(String[] args) throws IOException{
 	SpiritTest obj = new SpiritTest();
 	obj.LoadPage();
-	obj.selectTripType("Round Trip");
-	obj.selectOrigin("ATL");s
-//	obj.selectDest("SAN");
+	obj.selectTripType("One Way");
+	obj.selectOrigin("PBI");
+	obj.selectDest("ORD");
+	obj.selectDate("12/02/2020");
 }
 }
--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,6 +1,10 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 public class TestClass extends BaseClass {
 	static boolean flag; 
@@ -31,14 +35,44 @@ public static void searchItem(String item) throws IOException {
 	passKeys(getWebelement("//input[@class='form-control siteSearch__field']"), Keys.ENTER);
 	CaptureScreen("Searched-"+item);
 }
-public static void addtoCart() {
-	
+public static void addtoCart(String item, String qty) throws InterruptedException {
+	Thread.sleep(5000);
+	List<WebElement> items = getWebelements("//a[@class='quickview hidden-md-down button--outline']");
+	for (WebElement x : items) {
+		if(x.getAttribute("data-productname").equals(item)) {
+//			System.out.println(x.getAttribute("data-productname"));
+			x.findElement(By.xpath("./..")).click();
+			break;
+		}
+	}
+	int quantity = Integer.parseInt(qty);
+	for(int i = 0; i<quantity-1;i++) {
+		Clickon(getWebelement("//button[@aria-label='Increase Quantity']"));
+		Thread.sleep(2000);
+	}
+	Clickon(getWebelement("//button[@type='submit']"));
 }
+public static void checkBag() throws InterruptedException {
+	hover(getWebelement("//a[@title='View Cart']"));
+	Thread.sleep(3000);
+	Clickon(getWebelement("//a[@class='checkout-btn']"));
+}
+public static void outToExcel() throws IOException {
+	List<String> vals = new ArrayList<String>();
+	List<WebElement> bagItems = getWebelements("//div[@class='line-item-name']/a/span");
+	for (WebElement x : bagItems) {
+		vals.add(x.getText());
+	}
+	vals.add(getWebelement("//div[@class='cart-summary__value grand-total']").getText());
+	writetoExcel(vals);
+}
+
 public static void main(String[] args) throws IOException, InterruptedException {
 	loadTestpage();
 	signIn();
 	searchItem(getData(1,0));
-	searchItem(getData(2,0));
-	searchItem(getData(3,0));
+	addtoCart(getData(1,1),getData(1,2));
+	checkBag();
+	outToExcel();
 }
 }
